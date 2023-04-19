e73df3468e852914487b4bc6592e3b2c48dad562
JayParis <jay.paris32@gmail.com>
2023-04-19 12:48:43 +0100

    Version update and colour change

--- barScript.js
+++ barScript.js
@@ -152,7 +152,7 @@ BarScript.prototype.resizeBar = function(){
     text.element.text = device.height;
     uiBox2.findComponents("element")[1].text = document.getElementById('application').offsetHeight;
 
-    mat.color.set(0.16,0,0.5);
+    mat.color.set(0.16,0,0.95);
     mat.update();
     console.log("Colour set: " + mat.color);
     
